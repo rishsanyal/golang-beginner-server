04a76d0fd9c539e0ef2ac60efbeb7e9b03b3792d
Rishab Sanyal <rsanyal@ucdavis.edu>
2023-10-12 00:10:11 -0700

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -9,38 +9,41 @@ import (
 	"strings"
 )
 
-// func sendResponse(res string, conn net.Conn) {
-// 	conn.Write([]byte(res + "\n"))
-// }
+func handleConnection(conn net.Conn) {
 
-// func handleRequest(conn net.Conn) {
-// 	for {
-// 		log.Println("Handling Request")
-// 		buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024)
+	length, err := conn.Read(buffer)
+	if err != nil {
+		log.Panicln(err)
+	}
 
-// 		length, err := conn.Read(buffer)
-// 		if err != nil {
-// 			log.Panicln(err)
-// 		}
+	str := string(buffer[:length])
 
-// 		str := string(buffer[:length])
+	result := strings.Split(str, " ")
 
-// 		fmt.Println(conn.RemoteAddr().String())
-// 		fmt.Printf("Received command %d\t:%s\n", length, str)
+	// fmt.Println(result)
 
-// 		switch str {
-// 		case "PING\r\n":
-// 			sendResponse("PONG", conn)
-// 		case "PUSH\r\n":
-// 			sendResponse("GOT PUSH", conn)
-// 		case "QUIT\r\n":
-// 			sendResponse("Goodbye", conn)
-// 			conn.Close()
-// 		default:
-// 			conn.Write([]byte(fmt.Sprintf("UNKNOWN_COMMAND: %s\n", str)))
-// 		}
-// 	}
-// }
+	path := result[1]
+
+	switch {
+	case path == "/":
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	case strings.HasPrefix(path, "/echo"):
+		pathQuery := strings.Split(path, "/echo/")
+		resultStr := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(pathQuery[1])) + "\r\n\n" + pathQuery[1] + "\r\n"
+		conn.Write([]byte(resultStr))
+	case strings.HasPrefix(path, "/user-agent"):
+		userAgentInfo := strings.Split(str, "User-Agent: ")
+		userAgentInfo = strings.Split(userAgentInfo[1], "\n")
+		tempUserAgentInfo := strings.TrimSpace(userAgentInfo[0])
+		resultStr := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(tempUserAgentInfo)) + "\r\n\n" + tempUserAgentInfo + "\n"
+		conn.Write([]byte(resultStr))
+	default:
+		conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
+	}
+
+	conn.Close()
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -54,46 +57,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	var conn, error = l.Accept()
-	if error != nil {
-		fmt.Println("Error accepting connection: ", error.Error())
-		os.Exit(1)
-	} else {
-		buffer := make([]byte, 1024)
-		length, err := conn.Read(buffer)
-		if err != nil {
-			log.Panicln(err)
+	for {
+
+		var conn, error = l.Accept()
+		if error != nil {
+			fmt.Println("Error accepting connection: ", error.Error())
+			os.Exit(1)
+		} else {
+			go handleConnection(conn)
 		}
 
-		str := string(buffer[:length])
-
-		result := strings.Split(str, " ")
-
-		// fmt.Println(result)
-
-		path := result[1]
-
-		// fmt.Println(conn.RemoteAddr().String())
-		// fmt.Printf("Received command %d\t:%s\n", length, str)
-
-		switch {
-		case path == "/":
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		case strings.HasPrefix(path, "/echo"):
-			pathQuery := strings.Split(path, "/echo/")
-			resultStr := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(pathQuery[1])) + "\r\n\n" + pathQuery[1] + "\r\n"
-			conn.Write([]byte(resultStr))
-		case strings.HasPrefix(path, "/user-agent"):
-			userAgentInfo := strings.Split(str, "User-Agent: ")
-			userAgentInfo = strings.Split(userAgentInfo[1], "\n")
-			tempUserAgentInfo := strings.TrimSpace(userAgentInfo[0])
-			// fmt.Println(userAgentInfo[0])
-			resultStr := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(tempUserAgentInfo)) + "\r\n\n" + tempUserAgentInfo + "\n"
-			conn.Write([]byte(resultStr))
-		default:
-			conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
-		}
 	}
 
-	conn.Close()
 }
